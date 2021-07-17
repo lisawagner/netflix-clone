f4b33371576aa7100bae8991c3d6fd41434efe95
Lisa Wagner <lisawagner.online@gmail.com>
2021-07-17 16:21:11 -0700

    added footer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
-import "normalize.css";
 import App from "./app";
+
+import "normalize.css";
 import { GlobalStyles } from "./global-styles";
 
 render(
