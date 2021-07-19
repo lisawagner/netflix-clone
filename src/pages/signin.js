56c884f45261b7054f086078fc68d85ebcfe17e5
Lisa Wagner <lisawagner.online@gmail.com>
2021-07-19 12:05:10 -0700

    added signin form page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
+import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
+import { Form } from "../components";
 
 export default function Signin() {
-  return <p>Hello from the sign in component</p>;
+  const { firebase } = useContext(FirebaseContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  // check for valid inputs
+  const isInvalid = password === "" || email === "";
+  const handleSignIn = (event) => {
+    event.preventDefault();
+
+    // firebase work here
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTHCA to ensure your're not a
+            bot. Learn More.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />;
+    </>
+  );
 }
