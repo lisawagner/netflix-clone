ec2cca961c4818b248783722dfa554b24c7fb988
Lisa Wagner <lisawagner.online@gmail.com>
2021-07-19 14:45:27 -0700

    created user routes and authorized user hook

---
+++ src/helpers/routes.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+// if we have a user, direct them to the correct page.
+// if not user, route them to the correct page.
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (!user) {
+          return children;
+        }
+
+        if (user) {
+          return (
+            <Redirect
+              to={{
+                pathname: loggedInPath,
+              }}
+            />
+          );
+        }
+
+        return null;
+      }}
+    />
+  );
+}
+
+// if not a user, protect pages and send them back to signin
+export function ProtectedRoute({ user, children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        if (user) {
+          return children;
+        }
+
+        if (!user) {
+          return (
+            <Redirect
+              to={{
+                pathname: "signin",
+                state: { from: location },
+              }}
+            />
+          );
+        }
+
+        return null;
+      }}
+    />
+  );
+}
