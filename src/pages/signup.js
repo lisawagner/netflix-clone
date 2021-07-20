56f21ca0e9801622c5c176a28ebbc80c7dd2b143
Lisa Wagner <lisawagner.online@gmail.com>
2021-07-20 09:21:42 -0700

    bug fix

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -6,7 +6,7 @@ import { FooterContainer } from "../containers/footer";
 import { Form } from "../components";
 import * as ROUTES from "../constants/routes";
 
-export default function Signup() {
+export default function SignUp() {
   const history = useHistory();
   const { firebase } = useContext(FirebaseContext);
 
